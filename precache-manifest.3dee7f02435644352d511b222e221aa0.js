d6f9aefd6b15ab456b6c04e37b3b9953e4c155ce
Aniket Kumar Verma <vermaaniket213@gmail.com>
2020-09-07 13:14:55 +0530

    Updates

---
+++ precache-manifest.3dee7f02435644352d511b222e221aa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6444d7e55ae1615bdfb463b11d360df3",
+    "url": "/second/index.html"
+  },
+  {
+    "revision": "7d0bb4f791814a6daa37",
+    "url": "/second/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a863ff4ee57a481290b5",
+    "url": "/second/static/js/2.549dab81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/second/static/js/2.549dab81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0bb4f791814a6daa37",
+    "url": "/second/static/js/main.3dadd972.chunk.js"
+  },
+  {
+    "revision": "486f4ff7bf56968a4175",
+    "url": "/second/static/js/runtime-main.b69f2b26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/second/static/media/logo.5d5d9eef.svg"
+  }
+]);
